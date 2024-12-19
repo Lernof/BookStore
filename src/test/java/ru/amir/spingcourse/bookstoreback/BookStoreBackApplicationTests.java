b4a195afbb22bc8ee798a939ce455e2817f567cc
Amir <168825860+AmirAbdykeyev@users.noreply.github.com>
2024-12-19 11:50:34 +0500

    adding unit and api tests

--- src/test/java/ru/amir/spingcourse/bookstoreback/BookStoreBackApplicationTests.java
+++ src/test/java/ru/amir/spingcourse/bookstoreback/BookStoreBackApplicationTests.java
@@ -1,13 +1,31 @@
 package ru.amir.spingcourse.bookstoreback;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.amir.spingcourse.bookstoreback.models.Book;
+import ru.amir.spingcourse.bookstoreback.repositories.BooksRepository;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BookStoreBackApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private BooksRepository booksRepository;
 
+    @Test
+    void testCreateAndFindBook() {
+        Book newBook = new Book("My New Book", "Author Name", new Date());
+
+        Book savedBook = booksRepository.save(newBook);
+
+        assertNotNull(savedBook.getId(), "Saved book should have a generated ID");
+
+        Book foundBook = booksRepository.findById(savedBook.getId()).orElse(null);
+
+        assertNotNull(foundBook, "Book should be found in DB");
+    }
 }
